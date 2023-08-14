e99fce1c6fff19d13b64f84eb2de046039cbd348
Pavan Kusnuri <pavantejakusunuri@gmail.com>
2023-08-14 17:25:50 +0530

    Added Core Components for Portfolio Project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Pavan &nbsp;
-            <span className='sm:block hidden'> | Kusunuri</span>
+            Adrian &nbsp;
+            <span className='sm:block hidden'> | JavaScript Mastery</span>
           </p>
         </Link>
 
